e21ee8819cc0c670ae5bf0ceb937ba80cce774a6
ruk <ruk.lakhani@students.makeschool.com>
2020-10-08 15:14:02 -0700

    mood shop

---
+++ mood-shop/resources/js/scripts.js
@@ -0,0 +1,42 @@
+import data from './data.js'
+
+const itemsContainer = document.getElementById('items')
+
+// the length of our data determines how many times this loop goes around
+for (let i=0; i<data.length; ++i) {
+    // create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    // create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    // Add the image to the div
+    newDiv.appendChild(img)
+    console.log(img)
+}
+
+// the length of our data determines how many times this loop goes around
+for (let i=0; i<data.length; ++i) {
+    // create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    // create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    // Add the image to the div
+    newDiv.appendChild(img)
+
+
+    // put new div inside items container
+    itemsContainer.appendChild(newDiv)
+}
