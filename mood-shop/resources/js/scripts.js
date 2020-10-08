41d348c11a69437189c4142b821d48e699e00583
ruk <ruk.lakhani@students.makeschool.com>
2020-10-08 16:50:34 -0700

    displayed moods with button using javascript

--- mood-shop/resources/js/scripts.js
+++ mood-shop/resources/js/scripts.js
@@ -2,23 +2,6 @@ import data from './data.js'
 
 const itemsContainer = document.getElementById('items')
 
-// the length of our data determines how many times this loop goes around
-for (let i=0; i<data.length; ++i) {
-    // create a new div element and give it a class name
-    let newDiv = document.createElement('div');
-    newDiv.className = 'item'
-
-    // create an image element
-    let img = document.createElement('img');
-    // this will change each time we go through the loop. Can you explain why?
-    img.src = data[i].image
-    img.width = 300
-    img.height = 300
-
-    // Add the image to the div
-    newDiv.appendChild(img)
-    console.log(img)
-}
 
 // the length of our data determines how many times this loop goes around
 for (let i=0; i<data.length; ++i) {
@@ -36,7 +19,23 @@ for (let i=0; i<data.length; ++i) {
     // Add the image to the div
     newDiv.appendChild(img)
 
+    // create a paragraph element for a description
+    let desc = document.createElement('P')
+    // give the paragraph text from the data
+    desc.innerText =data[i].desc
+    // append the paragraph to the div
+    newDiv.appendChild(desc)
+    // do the same thing for price
+    let price = document.createElement('P')
+    let button = document.createElement('button')
+    button.id = data[i].name
+    button.dataset.price = data[i].price
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
 
     // put new div inside items container
     itemsContainer.appendChild(newDiv)
+    
 }
